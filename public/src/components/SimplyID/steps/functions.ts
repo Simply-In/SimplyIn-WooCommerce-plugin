73521aceb4fd6b3f466e9d8337b906af19931ae5
Grzegorz Zych <grzegorzzych95@gmail.com>
2024-03-25 14:23:42 +0100

    SIN-189-same-address-autofill-management-updated

--- public/src/components/SimplyID/steps/functions.ts
+++ public/src/components/SimplyID/steps/functions.ts
@@ -4,3 +4,23 @@ import placeholder from '../../../assets/placeholder.png';
 export const getPlaceholder = () => {
 	return placeholder
 }
+
+
+type addressType = { [key: string]: string }
+
+type isSameShippingAndBillingAddressesType = {
+	billingAddress: addressType,
+	shippingAddress: addressType,
+}
+
+export const isSameShippingAndBillingAddresses = ({ billingAddress, shippingAddress }: isSameShippingAndBillingAddressesType): boolean => {
+
+	const comparingKeys = Object.keys(shippingAddress).filter((key) => key !== "_id")
+
+	for (const key of comparingKeys) {
+		if (shippingAddress[key] !== billingAddress[key]) {
+			return false
+		}
+	}
+	return true
+}
