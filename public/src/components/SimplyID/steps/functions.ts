60b4995e2d6ac52a651fcb7a074b9c4a050f9ceb
grom1717 <50084927+ZychGrzegorz@users.noreply.github.com>
2024-04-04 17:02:48 +0200

    SIN-204/feature/filling shipping address when it was not selected (#4)

--- public/src/components/SimplyID/steps/functions.ts
+++ public/src/components/SimplyID/steps/functions.ts
@@ -15,7 +15,7 @@ type isSameShippingAndBillingAddressesType = {
 
 export const isSameShippingAndBillingAddresses = ({ billingAddress, shippingAddress }: isSameShippingAndBillingAddressesType): boolean => {
 
-	const comparingKeys = Object.keys(shippingAddress).filter((key) => key !== "_id")
+	const comparingKeys = Object.keys(shippingAddress || {}).filter((key) => key !== "_id")
 
 	for (const key of comparingKeys) {
 		if (shippingAddress[key] !== billingAddress[key]) {
