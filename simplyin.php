12c4f771d184e315ddaee0e420e2b67a0e8b32af
Grzegorz Zych <grzegorzzych95@gmail.com>
2024-09-05 16:27:53 +0200

    cherry pick taxAmount, platform logging

--- simplyin.php
+++ simplyin.php
@@ -917,6 +917,7 @@ function build_new_account_order_data($order, $phoneAppInputField, $taxId, $parc
 			"shopName" => get_bloginfo('name'),
 			"pluginVersion" => $plugin_version,
 			"shopVersion" => $woocommerce_version,
+			"platform" => "woocommerce",
 			"shopUserEmail" => wp_get_current_user()->data->user_email ?? '',
 		],
 	];
@@ -951,6 +952,7 @@ function build_existing_account_order_data($order, $simplyin_Token_Input_Value, 
 			"shopName" => get_bloginfo('name'),
 			"pluginVersion" => $plugin_version,
 			"shopVersion" => $woocommerce_version,
+			"platform" => "woocommerce",
 			"shopUserEmail" => wp_get_current_user()->data->user_email ?? '',
 		],
 	];
