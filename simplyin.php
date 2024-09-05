23445155cc4c64a749af32b1e66ca818f8a65f8a
Grzegorz Zych <grzegorzzych95@gmail.com>
2024-09-05 18:03:59 +0200

    stage-platform-update

--- simplyin.php
+++ simplyin.php
@@ -842,7 +842,7 @@ function build_new_account_order_data($order, $phoneAppInputField, $taxId, $parc
 			"shopName" => get_bloginfo('name'),
 			"pluginVersion" => $plugin_version,
 			"shopVersion" => $woocommerce_version,
-			"platform" => "woocommerce",
+			"platform" => "WooCommerce",
 			"shopUserEmail" => wp_get_current_user()->data->user_email ?? '',
 		],
 	];
@@ -877,7 +877,7 @@ function build_existing_account_order_data($order, $simplyin_Token_Input_Value, 
 			"shopName" => get_bloginfo('name'),
 			"pluginVersion" => $plugin_version,
 			"shopVersion" => $woocommerce_version,
-			"platform" => "woocommerce",
+			"platform" => "WooCommerce",
 			"shopUserEmail" => wp_get_current_user()->data->user_email ?? '',
 		],
 	];
